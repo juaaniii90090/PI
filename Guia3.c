f4880900fada334b1f7ee2d79bcd28e0e38de8ce
juaaniii90090 <juani200107@gmail.com>
2026-03-18 14:36:37 +0000

    modifico archivo

--- Guia3.c
+++ Guia3.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 #include "getnum.h"
 
+void factorizacionPrima(int n){
+    if (n < 2) {
+        printf("%d No tiene factorizacion Prima \n",n);
+        return;
+    }
+    printf("%d = ", n);
+
+    while (n % 2 == 0)
+    {
+        printf("2");
+        n /= 2;
+        if (n > 1) printf(" * ");
+    }
+
+    for (int i = 3; i*i <= n; i+= 2) {
+        while (n % i == 0){
+            printf ("%d", i);
+            n /= i;
+            if (n > 1) printf (" * ");
+        }
+    }
+    if ( n > 2 ){
+        printf("%d", n);
+    }
+    printf("\n");
+}
+
 int esPrimo(unsigned int num){
     int div = 0;
     if (num < 2) return 0;
@@ -29,6 +56,5 @@ void nPrimos(unsigned int cantidadPrimos){
 
 }
 int main (void){
-    nPrimos(20);
-    printf("\n");
+    factorizacionPrima(8293813);
 }
---
+++ Numerosamigos.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "getnum.h"
+
+int Namigo(unsigned int N1, unsigned int N2){
+
+int div1 = 0;
+int div2 = 0; 
+
+for(int i=1 ; i < N1 ; i++){
+    if(N1 % i == 0){
+        div1 += i;
+    }
+}
+
+for(int i=1 ; i < N2 ; i++){
+    if(N2 % i == 0){
+        div2 += i;
+    }
+}
+
+
+if(N2 == div1 && N1 == div2){
+    printf("Son amigos");
+    return 1;
+}
+else{
+    printf("Son enemigos");
+    return 0;    
+}
+}
+
+int main(void){
+    Namigo(220, 284);
+    
+}
